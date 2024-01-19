44a1fee34c179f9eedf726632b196325d818a3fd
Gergely Bódi <vendelin8@gmail.com>
2024-01-19 22:32:31 +0100

    imlemented db layer

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.21.6
 require (
 	github.com/gofiber/fiber/v2 v2.52.0
 	github.com/gofiber/swagger v0.1.14
+	github.com/google/uuid v1.5.0
+	github.com/redis/go-redis/v9 v9.4.0
 	github.com/swaggo/swag v1.16.2
 	github.com/valyala/fasthttp v1.51.0
 )
@@ -14,11 +16,12 @@ require (
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.19.6 // indirect
 	github.com/go-openapi/spec v0.20.4 // indirect
 	github.com/go-openapi/swag v0.19.15 // indirect
-	github.com/google/uuid v1.5.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
--- internal/db/db.go
+++ internal/db/db.go
@@ -3,13 +3,27 @@ package db
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 
+	"github.com/google/uuid"
+	"github.com/redis/go-redis/v9"
 	"github.com/vendelin8/card-fun/pkg/deck"
+	"github.com/vendelin8/card-fun/pkg/util"
+)
+
+const (
+	// maxRetries is the number of retries for Redis transaction optimistic locking.
+	maxRetries = 1000
+	// detailsFmt is how deck details is stored where %s is the deck id.
+	dataFmt = "data%s"
 )
 
 var (
+	rdb *redis.Client
+
 	ErrMissingDeck = errors.New("This deck cannot be found")
 	ErrMaxRetries  = errors.New("Reached maximum number of retries")
 )
@@ -23,20 +37,129 @@ func (e ErrDeckLen) Error() string {
 	return fmt.Sprintf("Deck has less (%d) cards then requested (%d)", e.Remaining, e.Requested)
 }
 
+func init() {
+	redisAddr := util.GetEnv("REDIS_URL", "localhost:6379")
+	rdb = redis.NewClient(&redis.Options{Addr: redisAddr, Password: "", DB: 0})
+	if err := rdb.Ping(context.Background()).Err(); err != nil {
+		log.Printf("Error trying to ping redis: %v", err)
+	}
+}
+
 // StoreDeck stores the given cards in a new deck.
 func StoreDeck(ctx context.Context, d *deck.Deck) error {
-	fmt.Println("db.StoreDeck") // TODO: implement
+	pipe := rdb.Pipeline()
+	var deckID string
+	res := int64(1)
+	for res > 0 {
+		u, err := uuid.NewRandom()
+		if err = handleError("create random uuid", err); err != nil {
+			return err
+		}
+		deckID = u.String()
+		res, err = pipe.Exists(ctx, deckID).Result()
+		if err = handleError("check deck existance", err); err != nil {
+			return err
+		}
+	}
+	ifCodes := make([]interface{}, len(d.Codes))
+	for i, c := range d.Codes {
+		ifCodes[i] = c
+	}
+	pipe.RPush(ctx, deckID, ifCodes...)
+	b, err := json.Marshal(d.Data)
+	if err = handleError("JSON encode data", err); err != nil {
+		return err
+	}
+	pipe.Set(ctx, fmt.Sprintf(dataFmt, deckID), string(b), 0)
+	_, err = pipe.Exec(ctx)
+	if err = handleError("create deck", err); err != nil {
+		return err
+	}
+	d.ID = deckID
 	return nil
 }
 
 // All returns a deck of cards for the given deck.
 func All(ctx context.Context, d *deck.Deck) error {
-	fmt.Println("db.All") // TODO: implement
+	codes, err := rdb.LRange(ctx, d.ID, 0, -1).Result()
+	if err = handleError("read deck", err); err != nil {
+		return err
+	}
+	if len(codes) == 0 {
+		return ErrMissingDeck
+	}
+	dataS, err := rdb.Get(ctx, fmt.Sprintf(dataFmt, d.ID)).Result()
+	if err = handleError("read deck data", err); err != nil {
+		return err
+	}
+	var data deck.Data
+	if err = handleError("JSON decode deck data", json.Unmarshal([]byte(dataS), &data)); err != nil {
+		return err
+	}
+	d.Data = data
+	d.Codes = codes
+	d.Remaining = len(codes)
 	return nil
 }
 
 // Draw draws the top n cards of a deck for the given deck.
 func Draw(ctx context.Context, d *deck.Deck, n int64) error {
-	fmt.Println("db.Draw") // TODO: implement
-	return nil
+	var lRangeRes *redis.StringSliceCmd
+	txf := func(tx *redis.Tx) error {
+		l, err := tx.LLen(ctx, d.ID).Result()
+		if err = handleError("get deck length", err); err != nil {
+			return err
+		}
+		if l == 0 {
+			return ErrMissingDeck
+		}
+		if l < n {
+			return ErrDeckLen{Remaining: int(l), Requested: int(n)}
+		}
+
+		// Operation is commited only if the watched keys remain unchanged.
+		_, err = tx.TxPipelined(ctx, func(pipe redis.Pipeliner) error {
+			lRangeRes = pipe.LRange(ctx, d.ID, 0, n-1)
+			pipe.LTrim(ctx, d.ID, n, -1)
+			if l == n {
+				pipe.Del(ctx, fmt.Sprintf(dataFmt, d.ID))
+			}
+			return nil
+		})
+		return handleError("draw multiple cards", err)
+	}
+
+	// Retry if the key has been changed.
+	for i := 0; i < maxRetries; i++ {
+		err := rdb.Watch(ctx, txf, d.ID)
+		if err == nil {
+			codes, err := lRangeRes.Result()
+			if err == nil {
+				d.Codes = codes
+				return nil
+			}
+		}
+		if errors.Is(err, redis.TxFailedErr) {
+			continue // optimistic lock lost, retry
+		}
+		return err
+	}
+
+	return ErrMaxRetries
+}
+
+func handleError(format string, err error, v ...any) error {
+	if err == nil {
+		return nil
+	}
+	format = fmt.Sprintf("%%v: failed to %s", format)
+	if len(v) == 0 {
+		log.Print(format)
+		return util.ErrUnknown
+	}
+	v2 := make([]any, len(v)+1)
+	v2[0] = err
+	copy(v2[1:], v)
+	log.Printf(format, v2...)
+	return util.ErrUnknown
 }
